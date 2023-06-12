6ea93c6c598e8ebf6cd472854b5d4d1e0cf72e2a
zadu009 <62472267+zadu009@users.noreply.github.com>
2023-06-13 00:29:45 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
+	build: {
+		sourcemap: false,
+		rollupOptions: {
+		  external:  [
+			'lodash.clonedeep', 
+			'lodash',
+		  ],
+		},
+	  },
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
